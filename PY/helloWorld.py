e715a386eb0bb7395c9a4ad4561e42c0e446ede9
efra8ab <efra8ab@gmail.com>
2025-04-05 07:44:50 -0600

    first program

---
+++ PY/helloWorld.py
@@ -0,0 +1,15 @@
+#Print “Hello, World!” and then ask the user for their name. 
+# Print a greeting using the name.
+
+import os
+
+def clear_screen():
+    os.system("clear")
+
+clear_screen()
+
+print("Hello World")
+name = input("What is your name?: ")
+
+clear_screen()
+print(f"Hello {name}")
