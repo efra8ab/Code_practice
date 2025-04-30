7e264831a4fb4079f3e318198bc8a53f4b274b5e
efra8ab <efra8ab@gmail.com>
2025-04-30 16:36:38 -0600

    changed sympy for prime check algo

--- PY/prime_num_between.py
+++ PY/prime_num_between.py
@@ -1,26 +1,42 @@
 #Ask the user to input a number N and then print all the prime numbers between 1 and N, inclusive
 
 import os
-from sympy import *
+
 
 def clear_screen():
     os.system('clear')
-    
 
-def prime_check(num):
-    num = abs(num)
-    for i in range (2, num + 1):
-        if isprime(i):
+#using algorithm 
+def manual_prime_check(num):
+    if num <= 1:
+        return False
+    
+    for i in range (2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def is_prime(num):
+    clear_screen()
+    print(f"Prime numbers between 0 - {num}")
+    for i in range(2, num + 1):
+        if manual_prime_check(i):
             print(f"{i}")
 
-try:
-    num = int(input("Enter a number: "))
-except ValueError:
-    print("Please enter a valid integer")
-    exit()
+while True:
+    clear_screen()
 
-clear_screen()
-prime_check(num)
+    try:
+        num = int(input("Enter a number: "))
+    except ValueError:
+        print("Please enter a valid integer")
+        exit()
+
+    is_prime(num)
+
+    response = input("\nDo you want to check another number? (y/n):")
+    if response.lower() == "n":
+        break
 
 
 
