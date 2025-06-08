2485be227cede69250245641b5dba1620061beb5
efra8ab <efra8ab@gmail.com>
2025-06-07 20:42:31 -0600

    web dev

---
+++ PY/prime_factor_finder.py
@@ -0,0 +1,7 @@
+#Write a program that:
+##Asks the user to enter a positive integer greater than 1.
+##Calculates and prints all prime factors of that number.
+
+for x in range(10):
+    for y in range(x):
+        print(y)
